2b1009b486e2498aea96a039ac845f2c5d7a1a33
Nicole Viviant <nicoleviviant@hotmail.com>
2025-06-24 00:40:06 -0300

    la ultima de la night'

--- sql/querys/SQLQuery1.sql
+++ sql/querys/SQLQuery1.sql
@@ -7,3 +7,17 @@ USE Formula1
 -- END 
 
 SELECT * FROM Drivers;
+
+SELECT * FROM GP;
+
+-- Query para renombrar columnas de la tabla Drivers y GP
+EXEC sp_rename 'Drivers.Driver_ID', 'DriverID', 'COLUMN';
+EXEC sp_rename 'Drivers.born', 'Born', 'COLUMN';
+EXEC sp_rename 'Drivers.nationality', 'Nationality', 'COLUMN';
+EXEC sp_rename 'Drivers.url', 'URL', 'COLUMN';
+
+EXEC sp_rename 'GP.Driver_ID', 'DriverID', 'COLUMN';
+EXEC sp_rename 'GP.year', 'Year', 'COLUMN';
+
+-- Le cambio el nombre a la tabla drivers a Drivers
+EXEC sp_rename 'drivers', 'Drivers';
